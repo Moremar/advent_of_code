5649b758e3b7fbb99f6244f462deaa4ea4e5698e
chevallier <chevallier.th@gmail.com>
2024-10-03 23:58:34 +0900

    AOC 2015 move from separate GitHub repo to common AOC repo

---
+++ aoc_2015/day04/script2.py
@@ -0,0 +1,13 @@
+from script1 import parse, hexa_hash
+
+
+def solve(secret_key):
+    i = 0
+    while True:
+        if hexa_hash(secret_key + str(i))[:6] == "000000":
+            return i
+        i += 1
+
+
+if __name__ == '__main__':
+    print(solve(parse("data.txt")))
---
+++ aoc_2015/day11/script2.py
@@ -0,0 +1,9 @@
+from script1 import parse, get_next_valid
+
+
+def solve(current_pwd):
+    return get_next_valid(get_next_valid(current_pwd))
+
+
+if __name__ == '__main__':
+    print(solve(parse("data.txt")))
