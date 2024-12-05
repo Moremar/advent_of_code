996c0c5cfd1eed7fbc3ee3f9daaa0fbbaeaf4816
chevallier <chevallier.th@gmail.com>
2024-12-05 14:51:40 +0900

    AOC 2024 day 5 - pylint cleanup

--- aoc_2024/day05/script.py
+++ aoc_2024/day05/script.py
@@ -16,38 +16,39 @@ def parse(file_name):
 
 
 def is_valid(update, rules):
+    """Check if an update is valid according to a set of rules"""
     return not any(rule[0] in update and
                    rule[1] in update and
                    update.index(rule[0]) > update.index(rule[1])
                    for rule in rules)
 
 
-def solve1(parsed):
+def solve1(rules_and_updates):
     """Solve part 1"""
-    rules, updates = parsed
+    rules, updates = rules_and_updates
     return sum(update[len(update) // 2] for update in updates if is_valid(update, rules))
 
 
-def solve2(parsed):
+def solve2(rules_and_updates):
     """Solve part 2"""
-    rules, updates = parsed
+    rules, updates = rules_and_updates
     invalid_rules = [update for update in updates if not is_valid(update, rules)]
 
     total = 0
-    for i in range(len(invalid_rules)):
-        while not is_valid(invalid_rules[i], rules):
+    for invalid_rule in invalid_rules:
+        while not is_valid(invalid_rule, rules):
             # find the next breached rule and move the 1st page of the rule right before the 2nd
             for rule in rules:
-                if rule[0] in invalid_rules[i] and rule[1] in invalid_rules[i]:
-                    idx1 = invalid_rules[i].index(rule[0])
-                    idx2 = invalid_rules[i].index(rule[1])
+                if rule[0] in invalid_rule and rule[1] in invalid_rule:
+                    idx1 = invalid_rule.index(rule[0])
+                    idx2 = invalid_rule.index(rule[1])
                     if idx2 < idx1:
-                        invalid_rules[i] = (invalid_rules[i][:idx2] +
-                                            [invalid_rules[i][idx1]] +
-                                            invalid_rules[i][idx2:idx1] +
-                                            invalid_rules[i][idx1 + 1:])
-            if is_valid(invalid_rules[i], rules):
-                total += invalid_rules[i][len(invalid_rules[i]) // 2]
+                        invalid_rule = invalid_rule[:idx2] + \
+                                       [invalid_rule[idx1]] + \
+                                       invalid_rule[idx2:idx1] + \
+                                       invalid_rule[idx1 + 1:]
+            if is_valid(invalid_rule, rules):
+                total += invalid_rule[len(invalid_rule) // 2]
                 break
     return total
 
