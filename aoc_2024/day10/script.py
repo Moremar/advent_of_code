38c1349fbfc4cac5be3dd1d5867afcf744496ce1
chevallier <chevallier.th@gmail.com>
2024-12-10 16:28:32 +0900

    AOC 2024 day 10

---
+++ aoc_2024/day10/script.py
@@ -0,0 +1,49 @@
+"""Solution for AOC 2024 day 10"""
+
+from collections import deque
+
+
+def parse(file_name):
+    """Parse the input file"""
+    with open(file_name, 'r', encoding='utf-8') as file:
+        world = {}
+        for (i, line) in enumerate(file.readlines()):
+            for (j, c) in enumerate(line.strip()):
+                world[(i, j)] = int(c)
+        return world
+
+
+def get_trailhead_summits(trailhead, world):
+    """Get a list of the summit of each trail starting at a given trailhead"""
+    summits = []
+    positions = deque([trailhead])
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    while len(positions) > 0:
+        pos = positions.popleft()
+        adjacent = [(pos[0] + direction[0], pos[1] + direction[1]) for direction in directions]
+        for next_position in adjacent:
+            if next_position in world and world[next_position] == world[pos] + 1:
+                if world[next_position] == 9:
+                    summits.append(next_position)
+                else:
+                    positions.append(next_position)
+    return summits
+
+
+def solve1(world):
+    """Solve part 1"""
+    return sum(len(set(get_trailhead_summits(trailhead, world)))
+               for trailhead in world
+               if world[trailhead] == 0)
+
+
+def solve2(world):
+    """Solve part 1"""
+    return sum(len(get_trailhead_summits(trailhead, world))
+               for trailhead in world
+               if world[trailhead] == 0)
+
+
+if __name__ == '__main__':
+    parsed = parse('data.txt')
+    print('Day 10:', solve1(parsed), solve2(parsed))
